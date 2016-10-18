b40d983bfed492cfef4ac9b02c8f5aee630dace8
alexstocks <dragoncharlie@foxmail.com>
2016-10-18 16:43:58 +0800

    del client

--- user-info/java-client/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# ******************************************************
-# EMAIL   : alexstocks@foxmail.com
-# FILE    : build.sh
-# ******************************************************
-
-mvn clean install -Dmaven.test.skip
--- user-info/java-client/src/main/java/com/ikurento/consumer/Gender.java
+++
@@ -1,9 +0,0 @@
-package com.ikurento.consumer;
-
-/**
- * Created by wuwen on 15/4/7.
- */
-public enum  Gender {
-    MAN,
-    WOMAN
-}
